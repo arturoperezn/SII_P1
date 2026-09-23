929202291c01014e8e5ac4e0e25252a5fd2f152c
Arturo Pérez <arturopereznoves@outlook.com>
2026-09-23 10:52:49 +0200

    Finalizadas las funciones de create, login y modify user

--- user.py
+++ user.py
@@ -6,34 +6,108 @@ app = Quart(__name__)
 
 # source .venv/bin/activate
 
-users = {}
-secret_uuid = uuid4()
-
-@app.put('/user')
-async def user():
-    data = await request.get_json()
-    user = data.get("name")
-    password = data.get("password")
-    
-    #vacio?
-    if not data or not user or "password" not in data:
-        return jsonify({"error": "Faltan parámetros requeridos"}), 400
-    
-    #comprobar si ya existe
-    if (user in users):
-        return jsonify({"error": "El usuario ya existe"}), 409
-    
-    #crear uid y token de este usuario
-    user_uid = str(uuid4())
-    token = str(uuid5(secret_uuid, user_uid))
-    
-    
-if __name__ == '__main__':
-    app.run(host='localhost', port=5050)
-
-
-
 """   curl -X PUT http://127.0.0.1:5050/user \
         -H "Content-Type: application/json" \
         -d '{"name": "alice", "password": "mi_password"}'
 """
+
+users = {}
+secret_uuid = uuid4()
+
+# funciones aux
+def hash_pwd (password: str) -> str:
+    #encode lo convierte a bits (sha256 necesita bits), sha256 encripta y hexdigest lo convierte a hex
+    return sha256(password.encode()).hexdigest()
+
+def check_login():
+    token = request.headers.get("Authorization")
+    if not token: 
+        return None
+            
+    token = token.replace("Bearer ", "")
+        
+    for user in users.values():
+        if user.get("token") == token:
+            return user
+    return None
+
+@app.put('/user')
+async def create_user():
+    
+    data = await request.get_json()
+    #vacio?
+    if not data:
+        return jsonify({"error": "Faltan parámetros requeridos"}), 400
+        
+    name = data.get("name")
+    password = data.get("password")
+    
+    if not name:
+            return jsonify({"error": "Falta name"}), 400
+    
+    if not password:
+                return jsonify({"error": "Falta password"}), 400
+    
+    #comprobar si ya existe
+    if (name in users):
+        return jsonify({"error": "El usuario ya existe"}), 409
+    
+    #crear uid y token de este usuario
+    user_uid = str(uuid4())
+    pwd_hash = hash_pwd(password)
+    token = str(uuid5(secret_uuid, user_uid))
+    
+    users[name] = {
+        "uid": user_uid,
+        "pwd_hash": pwd_hash,
+        "token": token
+    }
+    
+    return jsonify({
+        "uid": user_uid,
+        "token": token
+    }), 201
+
+    
+if __name__ == '__main__':
+    app.run(host='localhost', port=5050)
+
+@app.post("/user")
+async def login():
+    data = await request.get_json()
+    if not data:
+        return jsonify({"error": "Faltan datos"}), 400
+    
+    name = data.get("name")
+    password = data.get("password")
+    
+    #comprobamos si el usuario existe
+    user = users.get(name)
+    if not user:
+        return jsonify({"error": "Usuario no existe"}), 404
+    
+    if user["pwd_hash"] != hash_pwd(password):
+        return jsonify({"error": "Contraseña incorrecta"}), 401
+    
+    return jsonify({
+        "uid": user["uid"],
+        "token": user["token"]
+    })
+
+
+@app.patch("/user")
+async def modify_user():
+    #si no esta iniciada la sesion fallar
+    user = check_login()
+    if not user:
+        return jsonify({"error": "No autorizado"}), 401
+        
+    data = await request.get_json()
+    password = data.get("password")
+    
+    if not password:
+        return jsonify ({"error": "Falta nueva password"}), 400
+    
+    user ["pwd_hash"] = hash_pwd(password)
+    return jsonify ({"ok": True})
+    
